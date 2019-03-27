217b5ce2fae8d4d5753b6a669e067590f1897c8e
Franziskus Kiefer <franziskuskiefer@gmail.com>
2019-03-27 14:00:53 +0100

    toying

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,7 @@
 use crate::*;
 
 define_abstract_integer_checked!(BigBounded, 256);
+define_abstract_integer_checked!(BiggerBounded, 2048);
 
 #[test]
 #[should_panic]
@@ -14,6 +15,17 @@ define_refined_modular_integer!(Felem, BigBounded, BigBounded::pow2(255) - BigBo
 
 #[test]
 fn arith() {
+    let x1 = BigBounded::from_literal(24875808327634644);
+    let x2 = BigBounded::from_literal(91987276365379830);
+    let x3 = x1 + x2;
+    assert_eq!(BigBounded::from_literal(116863084693014474u128), x3.into());
+
+    let x1 = BiggerBounded::from_hex("10498385709182435134");
+    let x2 = BiggerBounded::from_hex("09871425437538592414723");
+    let x3 = x1 + x2;
+    assert_eq!(BiggerBounded::from_hex("98818bd7bcc41714849857"), x3.into());
+
+
     let x1 = Felem::from_literal(24875808327634644);
     let x2 = Felem::from_literal(91987276365379830);
     let x3 = x1 + x2;
