52e2641f03465a804b3bc632ddc3742c032c51ce
Franziskus Kiefer <franziskuskiefer@gmail.com>
2019-04-04 11:25:38 +0200

    cleanup

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,6 @@
 use crate::*;
 
 define_abstract_integer_checked!(BigBounded, 256);
-define_abstract_integer_checked!(BiggerBounded, 2048);
 
 #[test]
 #[should_panic]
@@ -24,10 +23,10 @@ fn arith() {
     let x3 = x1 + x2;
     assert_eq!(BigBounded::from_literal(116863084693014474u128), x3.into());
 
-    let x1 = BiggerBounded::from_hex("10498385709182435134");
-    let x2 = BiggerBounded::from_hex("09871425437538592414723");
+    let x1 = BigBounded::from_hex("10498385709182435134");
+    let x2 = BigBounded::from_hex("9871425437538592414723");
     let x3 = x1 + x2;
-    assert_eq!(BiggerBounded::from_hex("98818bd7bcc41714849857"), x3.into());
+    assert_eq!(BigBounded::from_hex("98818bd7bcc41714849857"), x3.into());
 
     let x1 = Felem::from_literal(24875808327634644);
     let x2 = Felem::from_literal(91987276365379830);
